5f67f36b33230d8ec7a81d69aed36de6b389d3b8
Bruno Ribeiro <brunoribeiro.sch@gmail.com>
2025-02-06 20:34:11 -0300

    Fix eslint

--- src/app/lista-compras/page.tsx
+++ src/app/lista-compras/page.tsx
@@ -1,38 +1,6 @@
-import Image from "next/image";
-import Link from "next/link";
-import path from "path";
 import { Tabela } from "./tabela";
 
-const features = [
-  {
-    nome: "Lista de Compras",
-    path: "lista-compras",
-  },
-  {
-    nome: "Inventário da casa",
-    path: "inventario",
-  },
-  {
-    nome: "Diretório de Pix",
-    path: "diretorio-pix",
-  },
-];
-
 export default function Home() {
-  function BotoesNavegacao(props: {
-    features: { nome: string; path: string }[];
-  }) {
-    return (
-      <div className="flex gap-4">
-        {props.features.map((feature) => (
-          <Link key={feature.path} href={"/" + feature.path} className="btn">
-            {feature.nome}
-          </Link>
-        ))}
-      </div>
-    );
-  }
-
   return (
     <main className="w-full h-screen flex flex-col p-8 items-start justify-start">
       <h1 className="text-5xl">Lista de compras</h1>
